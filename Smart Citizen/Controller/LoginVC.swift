9a9871342dbe56b7046c90398f6397eac3f5168c
Mustafa Hastürk <mustafa.hasturk@yandex.com>
2015-12-03 19:12:41 +0200

    add delegate for field on login scene

--- Smart Citizen/Controller/LoginVC.swift
+++ Smart Citizen/Controller/LoginVC.swift
@@ -8,15 +8,31 @@
 
 import UIKit
 
-class LoginVC: UIViewController {
+class LoginVC: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        emailField.delegate = self
+        passwordField.delegate = self
         
         self.navigationItem.title = "Login"
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+    
+    // press return
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    // outside of the TextField
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        self.view.endEditing(true)
     }
 }
