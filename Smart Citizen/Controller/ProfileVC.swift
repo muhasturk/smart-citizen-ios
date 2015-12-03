ab76ed4965a4dbdcf9e48477b30850327b2f3bb9
Mustafa Hastürk <mustafa.hasturk@yandex.com>
2015-12-03 18:26:22 +0200

    update constrains for login scene

--- Smart Citizen/Controller/ProfileVC.swift
+++ Smart Citizen/Controller/ProfileVC.swift
@@ -10,28 +10,12 @@ import UIKit
 
 class ProfileVC: UIViewController {
 
-    @IBOutlet weak var profileImage: UIImageView!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        profileImage.layer.cornerRadius = profileImage.frame.height/2
-        profileImage.clipsToBounds = true
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
