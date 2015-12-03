6cf548f1dc4552116b3c29551eff1e0734526c1a
Mustafa Hastürk <mustafa.hasturk@yandex.com>
2015-12-03 17:29:25 +0200

    add login and signup vc

---
+++ Smart Citizen/Controller/LoginVC.swift
@@ -0,0 +1,20 @@
+//
+//  LoginVC.swift
+//  Smart Citizen
+//
+//  Created by Mustafa Hastürk on 03/12/15.
+//  Copyright © 2015 Mustafa Hastürk. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
---
+++ Smart Citizen/Controller/SignUpVC.swift
@@ -0,0 +1,20 @@
+//
+//  SignUpVC.swift
+//  Smart Citizen
+//
+//  Created by Mustafa Hastürk on 03/12/15.
+//  Copyright © 2015 Mustafa Hastürk. All rights reserved.
+//
+
+import UIKit
+
+class SignUpVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
