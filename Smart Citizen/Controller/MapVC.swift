016184b6d5c347b0253c8916815bc9f94613a2b4
Mustafa Hastürk <mustafa.hasturk@yandex.com>
2015-12-04 04:54:31 +0200

    init map kit into map scene

--- Smart Citizen/Controller/MapVC.swift
+++ Smart Citizen/Controller/MapVC.swift
@@ -7,9 +7,12 @@
 //
 
 import UIKit
+import MapKit
 
-class MapVC: UIViewController {
+class MapVC: UIViewController, MKMapViewDelegate {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
