768bf6d06537f45198fe1015574950087cee832e
Mustafa Hastürk <mustafa.hasturk@yandex.com>
2015-12-04 02:13:27 +0200

    add ability to pick a photo from photolibrary

--- Smart Citizen/Controller/ReportVC.swift
+++ Smart Citizen/Controller/ReportVC.swift
@@ -8,12 +8,29 @@
 
 import UIKit
 
-class ReportVC: UIViewController {
+class ReportVC: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
 
+    @IBOutlet weak var choosenImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
+    @IBAction func pickImageAction(sender: AnyObject) {
+        let image = UIImagePickerController()
+        image.delegate = self
+        image.sourceType = UIImagePickerControllerSourceType.SavedPhotosAlbum
+        image.allowsEditing = false
+        
+        self.presentViewController(image, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage, editingInfo: [String : AnyObject]?) {
+        print("Image selected from Photo Library")
+        self.dismissViewControllerAnimated(true, completion: nil)
+        self.choosenImage.image = image
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
