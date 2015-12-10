1e2ddb5556daa95d3be1f357fb1122582e74a1d7
Mustafa Hastürk <mustafa.hasturk@yandex.com>
2015-12-10 02:59:39 +0200

    set versionLabel Dynamically from info.plist

--- Smart Citizen/Controller/SettingsVC.swift
+++ Smart Citizen/Controller/SettingsVC.swift
@@ -7,13 +7,30 @@
 //
 
 import UIKit
+import LTMorphingLabel
 
 class SettingsVC: UIViewController {
 
+    @IBOutlet weak var versionLabel: LTMorphingLabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.setVersionLabelDynamically()
     }
-
+    
+    // MARK: - Version Label
+    func setVersionLabelDynamically() -> Void {
+        if let path = NSBundle.mainBundle().pathForResource("Info", ofType: "plist"),
+            dict = NSDictionary(contentsOfFile: path) as? [String: AnyObject] {
+                if let version = dict["CFBundleShortVersionString"] {
+                    versionLabel.text = "Version: \(version as! String)"
+                }
+                else {
+                    print("CFBundleShortVersionString is missing on info.plist!")
+                }
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
